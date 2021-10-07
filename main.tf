31d2540cb1c634f0f4e8e1ea119f3868b02d2afd
小雨 <614422099@QQ.com>
2021-10-07 14:23:28 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,24 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+provider "docker" {}
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
   }
 }
